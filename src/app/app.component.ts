3f8dd69fdbce66a104eaa256ae9244b04c1b79cc
cbourget <charlesericbourget@gmail.com>
2017-04-20 15:10:33 -0400

    chore(igo2): include igo2 lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IgoMap, LayerService, XYZLayerOptions } from 'igo2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  public map = new IgoMap();
+  public mapView = {
+    projection: 'EPSG:3857',
+    center: [-70.545006, 46.056211],
+    zoom: 15
+  };
+
+  constructor(public layerService: LayerService) {}
+
+  ngOnInit() {
+    this.layerService.createAsyncLayer({
+      title: 'MSP Base Map',
+      type: 'xyz',
+      source: {
+        url: 'https://geoegl.msp.gouv.qc.ca/cgi-wms/mapcache.fcgi/tms/1.0.0/carte_gouv_qc_ro@EPSG_3857/{z}/{x}/{-y}.png'
+      }
+    } as XYZLayerOptions).subscribe(layer => this.map.addLayer(layer));
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+
+import { IgoModule } from 'igo2';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    MaterialModule.forRoot(),
+    IgoModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
