b63bbdc0a4d37bfa45af0d8a0c57f833513788b3
michael.jolin <michael.jolin@cauca.ca>
2017-04-21 11:25:41 -0400

    (feat) Add "additional-information" component

--- src/app/intervention-plan/intervention-plan.module.ts
+++ src/app/intervention-plan/intervention-plan.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {AdditionalInformationComponent} from './additional-information/additional-information.component';
 import {ProtocolComponent} from './protocol/protocol.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    FlexLayoutModule
   ],
   exports: [
-      ProtocolComponent
+    AdditionalInformationComponent,
+    ProtocolComponent,
+    FlexLayoutModule
   ],
   declarations: [
-      ProtocolComponent
+    AdditionalInformationComponent,
+    ProtocolComponent
   ]
 })
 export class InterventionPlanModule { }
