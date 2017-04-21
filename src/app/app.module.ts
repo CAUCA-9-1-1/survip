50c123c1963eef92ea0964efe0860e56875964da
michael.jolin <michael.jolin@cauca.ca>
2017-04-21 10:37:22 -0400

    (chore) Creation intervention page and move maps component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { IgoModule } from 'igo2';
@@ -29,6 +28,7 @@ import {
 
     SharedModule,
     FormModule,
+
     InterventionModule,
     InterventionRoutingModule,
     AppRoutingModule
