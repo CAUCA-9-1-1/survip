724904e46543f63dedd4e73caa291779299acddc
Eric Mercier <eric.mercier@cauca.ca>
2017-04-24 14:43:51 -0400

    (feat) Added a new component for survey contacts
    This is the frame of the survey contacts.  It shows, as of now, 2 empty cards.

--- src/app/survey/survey.module.ts
+++ src/app/survey/survey.module.ts
@@ -4,6 +4,7 @@ import {SharedModule} from '../shared/shared.module';
 import { BuildingComponent } from './building/building.component';
 import { BuildingDetailComponent } from './building-detail/building-detail.component';
 import { ImplantationPlanComponent } from './implantation-plan/implantation-plan.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   imports: [
@@ -12,12 +13,14 @@ import { ImplantationPlanComponent } from './implantation-plan/implantation-plan
   exports: [
     BuildingDetailComponent,
     BuildingComponent,
-    ImplantationPlanComponent
+    ImplantationPlanComponent,
+    ContactComponent
   ],
   declarations: [
     BuildingDetailComponent,
     BuildingComponent,
     ImplantationPlanComponent,
+    ContactComponent,
   ],
   providers: [
       ]
