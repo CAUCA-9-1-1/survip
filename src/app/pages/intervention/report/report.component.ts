946b2f6685721dbbdcb0247e4a50fb27c31f6dbf
michael.jolin <michael.jolin@cauca.ca>
2017-04-25 11:21:47 -0400

    (chore) Don't git cordova platforms

--- src/app/pages/intervention/report/report.component.ts
+++ src/app/pages/intervention/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WindowRefService } from '../../../shared/window-ref.service';
 
 @Component({
   selector: 'app-intervention-report',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  constructor(private windowRef: WindowRefService) {}
 
   ngOnInit() {
   }
 
+  takePicture() {
+    if (this.windowRef.nativeWindow.cordova) {
+      alert('on cordova');
+    } else {
+      alert('on browser');
+    }
+  }
 }
--- src/app/shared/window-ref.service.ts
+++ src/app/shared/window-ref.service.ts
@@ -4,9 +4,17 @@ function getWindow (): any {
   return window;
 }
 
+function getNavigator (): any {
+  return navigator;
+}
+
 @Injectable()
 export class WindowRefService {
   get nativeWindow (): any {
     return getWindow();
+  }
+
+  get nativeNavigator (): any {
+    return getNavigator();
   }
 }
