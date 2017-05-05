962521310e432e4b81fd53ee45fb06bf4c108453
frederic.catellier <frederic.catellier@cauca.ca>
2017-05-05 12:00:33 -0400

    (feat) ReactiveForms and auto save feature boiler plate codes added.

--- src/app/intervention-survey/dangerous-material/dangerous-material.component.ts
+++ src/app/intervention-survey/dangerous-material/dangerous-material.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 import {BuildingHazardousMaterial} from '../shared/models/building-hazardous-material';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {BuildingHazardousMaterialService} from '../shared/services/building-hazardous-material.service';
 
 @Component({
   selector: 'app-intervention-survey-dangerous-material',
@@ -7,16 +9,50 @@ import {BuildingHazardousMaterial} from '../shared/models/building-hazardous-mat
   styleUrls: ['./dangerous-material.component.styl']
 })
 export class DangerousMaterialComponent implements OnInit, OnChanges {
-
+  materialForm: FormGroup;
   @Input() item: BuildingHazardousMaterial;
   @Output() deleteClicked = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private matService: BuildingHazardousMaterialService,
+    private fb: FormBuilder) {
+    this.createForm();
+    this.startWatchingForm();
+  }
 
   ngOnInit() {
+    this.setValues();
   }
   ngOnChanges() {
     console.log('changed');
   }
 
+  private setValues() {
+    this.materialForm.patchValue(this.item);
+  }
+
+  createForm() {
+    this.materialForm = this.fb.group({
+    });
+  }
+
+  private startWatchingForm() {
+    this.materialForm.valueChanges
+      .debounceTime(500)
+      .subscribe(() => this.saveIfValid());
+  }
+
+  private saveIfValid() {
+    if (this.materialForm.valid && this.materialForm.dirty) {
+      this.saveForm();
+    }
+  }
+
+  private saveForm() {
+    console.log('saving mat...');
+    const formModel  = this.materialForm.value;
+    Object.assign(this.item, formModel);
+    this.matService.update(this.item)
+      .then(() => console.log('Mat saved.'));
+  }
 }
