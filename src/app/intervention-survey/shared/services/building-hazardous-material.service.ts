5673ff09a3569077edc79ba31dc588c4f3abddfa
frederic.catellier <frederic.catellier@cauca.ca>
2017-05-08 11:39:39 -0400

    (refactor) removed useless code.

--- src/app/intervention-survey/shared/services/building-hazardous-material.service.ts
+++ src/app/intervention-survey/shared/services/building-hazardous-material.service.ts
@@ -4,7 +4,6 @@ import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
-import { BuildingContact } from '../models/building-contact';
 import {BuildingHazardousMaterial} from '../models/building-hazardous-material';
 
 @Injectable()
