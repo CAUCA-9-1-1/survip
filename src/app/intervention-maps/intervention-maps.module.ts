d438e2267db830a0e2fd9170f7ebaeb06db9739c
michael.jolin <michael.jolin@cauca.ca>
2017-05-09 15:56:31 -0400

    (fix) Change basePath of provideContextServiceOptions

--- src/app/intervention-maps/intervention-maps.module.ts
+++ src/app/intervention-maps/intervention-maps.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 
+import { provideContextServiceOptions } from 'igo2';
+
 import { SharedModule } from '../shared/shared.module';
 import { InterventionLayerDirective } from './intervention-layer/intervention-layer.directive';
 import { InterventionService } from './shared/intervention.service';
@@ -20,6 +22,10 @@ import { RiskLevelService } from '../shared/services/risk-level.service';
     InterventionService,
     InspectionService,
     RiskLevelService,
+    provideContextServiceOptions({
+      basePath: './contexts',
+      contextListFile: '_contexts.json'
+    })
   ]
 })
 export class InterventionMapsModule { }
