768376f16c13fcef3afdede0571d366df7a132e2
michael.jolin <michael.jolin@cauca.ca>
2017-05-11 16:11:55 -0400

    (fix) Load course for only one intervention plan

--- src/app/intervention-survey/shared/services/intervention-plan-course.service.ts
+++ src/app/intervention-survey/shared/services/intervention-plan-course.service.ts
@@ -6,13 +6,16 @@ import {InterventionPlanCourse} from '../models/intervention-plan-course';
 
 @Injectable()
 export class InterventionPlanCourseService extends BaseService {
+  private url = 'api/intervention-plan-course';
 
   constructor(private http: Http) {
     super();
   }
 
-  getAll() {
-    return this.http.get('api/intervention-plan-course', this.authorization()).map((response: Response) => {
+  getAll(id?: string) {
+    const url = this.url + (id ? '?idInterventionPlan=' + id : '');
+
+    return this.http.get(url, this.authorization()).map((response: Response) => {
       const result = response.json();
 
       // this.isLogin(result, '/prevention/survey');
