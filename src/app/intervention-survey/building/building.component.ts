451b41ec801504a67d36f8e11b796bb93ffce87a
frederic.catellier <frederic.catellier@cauca.ca>
2017-05-11 14:56:26 -0400

    (feat) Hardcoded values have been removed from the BuildingComponent.

--- src/app/intervention-survey/building/building.component.ts
+++ src/app/intervention-survey/building/building.component.ts
@@ -1,5 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {InterventionPlan} from '../shared/models/intervention-plan';
+import {RiskLevel} from '../shared/models/risk-level';
+import {RiskLevelService} from '../../shared/services/risk-level.service';
+import {LaneService} from '../shared/services/lane.service';
+import {InterventionPlanBuildingForDisplay} from '../shared/models/intervention-plan-building-for-display';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {InterventionPlanService} from '../shared/services/intervention-plan.service';
 
 @Component({
   selector: 'app-intervention-survey-building',
@@ -8,62 +14,67 @@ import {InterventionPlan} from '../shared/models/intervention-plan';
 })
 export class BuildingComponent implements OnInit {
   @Input() plan: InterventionPlan;
-  /*buildings: any[] = [
-    {
-      'idBuilding' : 1,
-      'alias' : 'Résidence',
-      'die' : 627,
-      'height' : 20,
-      'storeyCount' : 2
-    },
-    {
-      'idBuilding' : 2,
-      'alias' : 'Grange-étable',
-      'die' : 0,
-      'height' : 20,
-      'storeyCount' : 1
-    },
-    {
-      'idBuilding' : 3,
-      'alias' : 'Remise à machinerie',
-      'die' : 456,
-      'height' : 20,
-      'storeyCount' : 1
-    },
-    {
-      'idBuilding' : 4,
-      'alias' : 'Remise à fumier',
-      'die' : 627,
-      'height' : 20,
-      'storeyCount' : 2
-    },
-    {
-      'idBuilding' : 5,
-      'alias' : 'Remise à machinerie',
-      'die' : 627,
-      'height' : 20,
-      'storeyCount' : 2
-    },
-    {
-      'idBuilding' : 6,
-      'alias' : 'Remise à machinerie',
-      'die' : 627,
-      'height' : 20,
-      'storeyCount' : 2
-    },
-    {
-      'idBuilding' : 7,
-      'alias' : 'Garage',
-      'die' : 627,
-      'height' : 20,
-      'storeyCount' : 2
-    }
-    ];*/
+  riskLevels: RiskLevel[];
+  planForm: FormGroup;
 
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private planService: InterventionPlanService,
+    public laneService: LaneService,
+    private riskLevelService: RiskLevelService) {
+    this.loadRiskLevels();
+    this.createForm();
+    this.startWatchingForm();
   }
 
+  get mainBuilding(): InterventionPlanBuildingForDisplay {
+    if (this.plan != null) {
+      const result = this.plan.buildings.filter(building => building.isParent === true);
+      if (result.length > 0) {
+        return result[0];
+      }
+    }
+    return null;
+  }
+
+  private startWatchingForm() {
+    this.planForm.valueChanges
+      .debounceTime(500)
+      .subscribe(() => this.saveIfValid());
+  }
+
+  loadRiskLevels() {
+    this.riskLevelService.getAll()
+      .subscribe(result => this.riskLevels = result);
+  }
+
+  ngOnInit() {
+    this.setValues();
+  }
+
+  createForm() {
+    this.planForm = this.fb.group({
+      idLaneTransversal: ['']
+    });
+  }
+
+  private setValues() {
+    if (this.plan != null) {
+      this.planForm.patchValue(this.plan);
+    }
+  }
+
+  private saveIfValid() {
+    if (this.planForm.valid && this.planForm.dirty) {
+      this.saveForm();
+    }
+  }
+
+  private saveForm() {
+    console.log('saving plan...');
+    const formModel  = this.planForm.value;
+    Object.assign(this.plan, formModel);
+    this.planService.update(this.plan)
+      .then(() => console.log('Plan saved.'));
+  }
 }
