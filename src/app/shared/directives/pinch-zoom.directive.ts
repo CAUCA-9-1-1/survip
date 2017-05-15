ef03e33451bb9c4d145b4c0901ef7ca826b6e348
michael.jolin <michael.jolin@cauca.ca>
2017-05-15 13:33:22 -0400

    (feat) Open fullscreen dialog with zoom image

--- src/app/shared/directives/pinch-zoom.directive.ts
+++ src/app/shared/directives/pinch-zoom.directive.ts
@@ -1,16 +1,22 @@
 import {Directive, ElementRef, Renderer2} from '@angular/core';
 
+import {DialogsService} from '../services/dialogs.service';
+
 @Directive({
   selector: '[appPinchZoom]'
 })
 export class PinchZoomDirective {
 
-  constructor(el: ElementRef, renderer: Renderer2) {
-    renderer.setStyle(el.nativeElement, 'cursor', 'pointer');
-    renderer.listen(el.nativeElement, 'click', this.fullZoom.bind(this));
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private dialog: DialogsService
+  ) {
+    this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
+    this.renderer.listen(this.el.nativeElement, 'click', this.fullZoom.bind(this));
   }
 
   fullZoom(e) {
-    console.log(e);
+    this.dialog.fullscreen('Zoom', '<img src="' + this.el.nativeElement.src + '" />');
   }
 }
