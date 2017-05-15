8718ed6d94dce8431dcf8ed07d8fd959b4817a3e
michael.jolin <michael.jolin@cauca.ca>
2017-05-15 15:23:28 -0400

    (wip) Fix flex column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { LanguageService } from 'igo2';
+import {WindowRefService} from './shared/services/window-ref.service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,18 @@ import { LanguageService } from 'igo2';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private language: LanguageService) {
+  constructor(
+    private language: LanguageService,
+    private windowRef: WindowRefService
+  ) {
     language.setLanguage('fr');
+    windowRef.nativeWindow.addEventListener('resize', this.onResize.bind(this));
   }
 
   ngOnInit() {
   }
 
+  private onResize() {
+    console.log(arguments);
+  }
 }
