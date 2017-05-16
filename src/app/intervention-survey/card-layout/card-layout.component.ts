13bfab29f863cb36cafbcc7cf1d72ec8bc6bb55e
frederic.catellier <frederic.catellier@cauca.ca>
2017-05-16 09:28:09 -0400

    (feat) CardLayout's new card is not fully sized anymore when on mobile.

--- src/app/intervention-survey/card-layout/card-layout.component.ts
+++ src/app/intervention-survey/card-layout/card-layout.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output, AfterViewInit} from '@angular/core';
 import {WindowRefService} from '../../shared/services/window-ref.service';
 import {Observable} from 'rxjs/Rx';
+import {current} from 'codelyzer/util/syntaxKind';
 
 @Component({
   selector: 'app-card-layout',
@@ -15,35 +16,76 @@ export class CardLayoutComponent implements OnInit, AfterViewInit {
   @Output() completionClick = new EventEmitter();
   @Output() addClick = new EventEmitter();
 
-  constructor(private windowRefService: WindowRefService) { }
+  currentMode: string;
+
+  constructor(
+    private windowRefService: WindowRefService,
+    private windowRef: WindowRefService,
+  ) { }
 
   ngOnInit() {
 
   }
 
   ngAfterViewInit() {
-    this.setNewCardSize();
-    Observable.timer(1000)
-      .subscribe(() => this.setNewCardSize());
+    if (this.windowRef.nativeWindow.innerWidth >= 700) {
+      this.currentMode = 'desktop';
+      this.setNewCardSizeForDesktop();
+      Observable.timer(1000).subscribe(() => this.setNewCardSizeForDesktop());
+    } else {
+      this.setNewCardSizeForMobile();
+    }
+
+    Observable.fromEvent(this.windowRef.nativeWindow, 'resize')
+      .debounceTime(500)
+      .subscribe(() => {
+        const newMode = this.getCurrentScreenMode();
+        if (newMode !== this.currentMode) {
+          this.currentMode = newMode;
+          this.setNewCardSize();
+        }
+      });
+  }
+
+  private getCurrentScreenMode() {
+    return (this.windowRef.nativeWindow.innerWidth >= 700) ? 'desktop' : 'mobile';
   }
 
   private setNewCardSize() {
+    if (this.currentMode === 'desktop') {
+      this.setNewCardSizeForDesktop();
+    } else {
+      this.setNewCardSizeForMobile();
+    }
+  }
+
+  private setNewCardSizeForDesktop() {
     const items = this.windowRefService.nativeDocument.getElementsByClassName('detail-card mat-card');
-    if (items.length > 1) {
-      const newCardHeight = items[0]['offsetHeight'];
-      const newCardWidth = items[0]['offsetWidth'];
-      const item = document.getElementById('new-card');
-      item.style.width = newCardWidth + 'px';
-      item.style.height = newCardHeight + 'px';
+    const item = document.getElementById('new-card');
+    if (items.length > 1 && item != null) {
+      item.style.width = items[0]['offsetWidth'] + 'px';
+      item.style.height = items[0]['offsetHeight'] + 'px';
       item.style.padding = '0px';
     }
   }
+
+  private setNewCardSizeForMobile() {
+    const item = document.getElementById('new-card');
+    if (item != null) {
+      item.style.width = '90%';
+      item.style.height = '100px';
+      item.style.removeProperty('padding');
+    }
+  }
+
   onDeleteClicked(value: any) {
     this.deleteClick.emit(value);
   }
+
   onCompleteSectionClicked(value: any) {
     this.completionClick.emit(value);
   }
+
   onAddItemClicked(value: any) {
     this.addClick.emit(value);
   }
