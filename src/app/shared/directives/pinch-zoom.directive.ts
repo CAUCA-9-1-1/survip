020777d91838bcb9dbf96fcc949f1cb682c13638
michael.jolin <michael.jolin@cauca.ca>
2017-05-18 11:25:25 -0400

    (feat) Add pinch zoom on popup of take-picture component

--- src/app/shared/directives/pinch-zoom.directive.ts
+++ src/app/shared/directives/pinch-zoom.directive.ts
@@ -1,14 +1,19 @@
 import {Directive, ElementRef, Renderer2} from '@angular/core';
+import * as Hammer from 'hammerjs';
 
 import {DialogsService} from '../services/dialogs.service';
+import {WindowRefService} from '../services/window-ref.service';
 
 @Directive({
   selector: '[appPinchZoom]'
 })
 export class PinchZoomDirective {
+  private lastScale = 1;
+  private lastPan = [0, 0];
 
   constructor(
     private el: ElementRef,
+    private windowRef: WindowRefService,
     private renderer: Renderer2,
     private dialog: DialogsService
   ) {
@@ -18,5 +23,31 @@ export class PinchZoomDirective {
 
   fullZoom(e) {
     this.dialog.fullscreen('Zoom', '<img src="' + this.el.nativeElement.src + '" />');
+
+    if (this.windowRef.nativeWindow.cordova) {
+      setTimeout(this.setPinch.bind(this), 100);
+    }
+  }
+
+  setPinch(e) {
+    const hammerTag = new Hammer(this.windowRef.nativeDocument.querySelectorAll('app-fullscreen-dialog img')[0]);
+
+    hammerTag.get('pinch').set({ enable: true });
+    hammerTag.on('pan', (e) => {
+      e.target.style.marginLeft = (this.lastPan[0] + e.deltaX) + 'px';
+      e.target.style.marginTop = (this.lastPan[1] + e.deltaY) + 'px';
+    });
+    hammerTag.on('panend', (e) => {
+      this.lastPan = [
+        (this.lastPan[0] + e.deltaX),
+        (this.lastPan[1] + e.deltaY)
+      ];
+    });
+    hammerTag.on('pinch', (e) => {
+      e.target.style.transform = 'scale(' + (this.lastScale * e.scale) + ',' + (this.lastScale * e.scale) + ')';
+    });
+    hammerTag.on('pinchend', (e) => {
+      this.lastScale *= e.scale;
+    });
   }
 }
