d7c24adff9ce46c54a168b6c4c63ea10db6ef3e3
Eric Mercier <eric.mercier@cauca.ca>
2017-05-18 10:53:06 -0400

    (chore) finished preparing mock data for demo mode

--- src/app/mockdata/building-data.ts
+++ src/app/mockdata/building-data.ts
@@ -5,7 +5,7 @@ export class BuildingData {
       isParent: true,
       alias: 'Résidence',
       address: '',
-      idImage: 'a3852cc7-4354-47c8-8917-7c861fe87f68',
+      idImage: '048de54d-3bcf-11e7-bf23-5254004f3384',
       idLane: '2cfae3aa-da6f-46fb-9ff2-21fa1bde1152',
       idRiskLevel: 'd251f211-ea12-44c5-9f9c-081e80841128',
       matricule: '98354671000000000'
@@ -22,21 +22,21 @@ export class BuildingData {
     },
     {
       id: 'b618acd6-dd0f-4839-b0b5-f47fc14c435e',
-      address: '123 27E RUE NORD',
+      address: '880 27E RUE NORD',
       idLane: '2cfae3aa-da6f-46fb-9ff2-21fa1bde1152',
       idRiskLevel: 'd251f211-ea12-44c5-9f9c-081e80841128',
       matricule: '98354671000000000'
     },
     {
       id: '2e922d74-72b3-4312-8976-c1778aa79541',
-      address: '550 175E RUE',
+      address: '855 175E RUE',
       idLane: '2cfae3aa-da6f-46fb-9ff2-21fa1bde1152',
       idRiskLevel: 'd251f211-ea12-44c5-9f9c-081e80841128',
       matricule: '98354671000000000'
     },
     {
       id: 'ab4417e3-008c-48a6-80e9-990d6c73b204',
-      address: '235 175 rue',
+      address: '875, 175E RUE',
       idLane: '2cfae3aa-da6f-46fb-9ff2-21fa1bde1152',
       idRiskLevel: 'd251f211-ea12-44c5-9f9c-081e80841128',
       matricule: '98354671000000000'
