266dc731c41cbcdaa5706bec3ddb45f90d7db800
michael.jolin <michael.jolin@cauca.ca>
2017-06-12 08:19:57 -0400

    (feat) Allow to insert, update and remove city and city type

--- src/app/management-address/shared/services/citytype.service.ts
+++ src/app/management-address/shared/services/citytype.service.ts
@@ -17,10 +17,21 @@ export class CityTypeService {
     });
   }
 
+  public create(cityType: CityType) {
+    return this.http.post(
+      'citytype',
+      JSON.stringify(cityType)
+    ).map((response: Response) => response.json());
+  }
+
   public update(cityType: CityType) {
     return this.http.put(
       'citytype',
       JSON.stringify(cityType),
     ).map((response: Response) => response.json());
+  }
+
+  public remove(idCityType: string) {
+    return this.http.delete('citytype/' + idCityType).map((response: Response) => response.json());
   }
 }
