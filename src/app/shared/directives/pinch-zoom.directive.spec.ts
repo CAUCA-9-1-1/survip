9a364e0b8456f9673a61efd895af7a4fe9cc1267
michael.jolin <michael.jolin@cauca.ca>
2017-07-04 16:56:45 -0400

    (refactor) Transfert some component to angular-lib

--- src/app/shared/directives/pinch-zoom.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PinchZoomDirective } from './pinch-zoom.directive';
-
-describe('PinchZoomDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PinchZoomDirective();
-    expect(directive).toBeTruthy();
-  });
-});
