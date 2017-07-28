a81edba841e71cbd20ea848de47f4109db77f080
michael.jolin <michael.jolin@cauca.ca>
2017-07-28 09:57:07 -0400

    feat(shared): Center the "page not found" component

---
+++ src/app/shared/components/page-not-found/page-not-found.component.spec.ts
@@ -0,0 +1,33 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {TestModule} from '../../../test.module';
+import {PageNotFoundComponent} from './page-not-found.component';
+
+describe('PageNotFoundComponent', () => {
+  let component: PageNotFoundComponent;
+  let fixture: ComponentFixture<PageNotFoundComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TestModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        // PageNotFoundComponent
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
