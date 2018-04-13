3d80919201f6c475dd03d022a71b0637b6a70398
michael.jolin <michael.jolin@cauca.ca>
2018-04-13 11:38:15 -0400

    fix: Use ngx-translate

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,21 +6,24 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatFormFieldModule, MatInputModule, MatTabsModule, MatToolbarModule } from '@angular/material';
+import { DxTabPanelModule, DxTextBoxModule } from 'devextreme-angular';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { DxTabPanelModule, DxTextBoxModule } from 'devextreme-angular';
+
 import { environment } from '../../environments/environment';
 //import {MenuComponent} from './components/menu/menu.component';
 //import {MenuItemComponent} from './components/menu-item/menu-item.component';*/
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import {MultilangComponent} from './components/multilang/multilang.component';
+import { MultilangComponent } from './components/multilang/multilang.component';
 //import {ToolbarBackComponent} from './components/toolbar-back/toolbar-back.component';
 //import {FilterByPipe} from './pipes/filter.pipe';
+
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
 }
+
 
 @NgModule({
     declarations: [
@@ -45,12 +48,12 @@ export function HttpLoaderFactory(http: HttpClient) {
         MatInputModule,
         MatTabsModule,
         MatToolbarModule,
-        TranslateModule,
-
         MultilangComponent,
         ToolbarComponent,
         //MenuComponent,
         //ToolbarBackComponent,
+
+        TranslateModule,
     ],
     imports: [
         BrowserModule,
@@ -65,6 +68,9 @@ export function HttpLoaderFactory(http: HttpClient) {
         MatInputModule,
         MatTabsModule,
         MatToolbarModule,
+        DxTabPanelModule,
+        DxTextBoxModule,
+
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
@@ -72,8 +78,6 @@ export function HttpLoaderFactory(http: HttpClient) {
                 deps: [HttpClient]
             }
         }),
-        DxTabPanelModule,
-        DxTextBoxModule,
     ],
     providers: [
         HttpClientModule,
