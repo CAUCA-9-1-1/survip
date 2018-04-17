4f37f9a38585d5f324d59c920a09bd6f4c24e2b4
michael.jolin <michael.jolin@cauca.ca>
2018-04-17 15:37:08 -0400

    refactor: Use the webapi with city

--- src/app/management-address/shared/models/city.model.ts
+++ src/app/management-address/shared/models/city.model.ts
@@ -1,9 +1,19 @@
-export class City {
-  idCity: string;
-  name: object;
-  isActive: boolean;
-  idCounty: string;
-  idCityType: string;
-  code: string;
-  code3Letter: string;
+import {WithLocalization} from '../../../shared/models/with-localization';
+
+
+export class City extends WithLocalization {
+    id: string;
+    code: string;
+    code3Letters: string;
+    emailAddress: string;
+    idCounty: string;
+    idCityType: string;
+    isActive: boolean;
+    createdOn: Date;
+
+    static fromJSON(data: object): City {
+        const city = new City();
+
+        return Object.assign(city, data);
+    }
 }
--- src/app/management-address/shared/models/citytype.model.ts
+++ src/app/management-address/shared/models/citytype.model.ts
@@ -4,6 +4,7 @@ import {WithLocalization} from '../../../shared/models/with-localization';
 export class CityType extends WithLocalization {
     id: string;
     isActive: boolean;
+    createdOn: Date;
 
     static fromJSON(data: object): CityType {
         const cityType = new CityType();
