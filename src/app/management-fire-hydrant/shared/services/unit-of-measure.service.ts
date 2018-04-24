5aa61a7c6831e3419ef8a0fad18beba0e9b39ac1
michael.jolin <michael.jolin@cauca.ca>
2018-04-24 10:02:39 -0400

    refactor: Use the new webapi with unitofmeasure

--- src/app/management-fire-hydrant/shared/services/unit-of-measure.service.ts
+++ src/app/management-fire-hydrant/shared/services/unit-of-measure.service.ts
@@ -1,37 +1,37 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable, Injector} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import 'rxjs/add/operator/catch';
 
 import {UnitOfMeasure} from '../models/unit-of-measure.model';
+import {RequestService} from '../../../shared/services/request.service';
 
 
 @Injectable()
-export class UnitOfMeasureService {
+export class UnitOfMeasureService extends RequestService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, injector: Injector) {
+        super(injector);
+    }
 
-  public getAll(type?: string) {
-    /*return this.http.get('unitofmeasure' + (type ? '/' + type : '')).map((response: Response) => {
-      const result = response.json();
+    getAll() {
+        return this.http.get<UnitOfMeasure[]>(this.apiUrl + 'UnitOfMeasure', {
+            headers: this.headers
+        }).catch((error: HttpErrorResponse) => this.error(error));
+    }
 
-      return result.data;
-    });*/
-  }
+    save(unit: UnitOfMeasure) {
+        return this.http.post(
+            this.apiUrl + 'UnitOfMeasure',
+            JSON.stringify(unit),
+            {
+                headers: this.headers
+            }
+        ).catch((error: HttpErrorResponse) => this.error(error));
+    }
 
-  public create(unit: UnitOfMeasure) {
-    /*return this.http.post(
-      'unitofmeasure',
-      JSON.stringify(unit)
-    ).map((response: Response) => response.json());*/
-  }
-
-  public update(unit: UnitOfMeasure) {
-    /*return this.http.put(
-      'unitofmeasure',
-      JSON.stringify(unit),
-    ).map((response: Response) => response.json());*/
-  }
-
-  public remove(idUnitOfMeasure: string) {
-    // return this.http.delete('unitofmeasure/' + idUnitOfMeasure).map((response: Response) => response.json());
-  }
+    remove(idUnitOfMeasure: string) {
+        return this.http.delete(this.apiUrl + 'UnitOfMeasure/' + idUnitOfMeasure, {
+            headers: this.headers
+        }).catch((error: HttpErrorResponse) => this.error(error));
+    }
 }
