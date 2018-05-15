dafea7687f462ed90b1cf35eac103eddf2bcf077
michael.jolin <michael.jolin@cauca.ca>
2018-05-15 09:59:31 -0400

    feat(#90): Allow to approve/refuse/cancel an inspection

--- src/app/inspection-approval/inspection-approval.component.ts
+++ src/app/inspection-approval/inspection-approval.component.ts
@@ -1,19 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {InspectionService} from '../inspection-dashboard/shared/services/inspection.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'app-inspection-approval',
     templateUrl: './inspection-approval.component.html',
-    styleUrls: ['./inspection-approval.component.scss']
+    styleUrls: ['./inspection-approval.component.scss'],
+    providers: [
+        InspectionService,
+    ]
 })
 export class InspectionApprovalComponent implements OnInit {
     selected = 'buildingDetails';
 
-    constructor() { }
+    private inspectionId: string;
+
+    constructor(
+        private activeRoute: ActivatedRoute,
+        private inspectionService: InspectionService
+    ) {
+        this.activeRoute.params.subscribe(param => {
+            this.inspectionId = param.idInspection;
+        });
+    }
 
     ngOnInit() {
     }
 
     sectionChange(section) {
         this.selected = section;
+    }
+
+    approve() {
+        this.inspectionService.approve(this.inspectionId).subscribe();
+    }
+
+    refuse() {
+        this.inspectionService.refuse(this.inspectionId).subscribe();
+    }
+
+    cancel() {
+        this.inspectionService.cancel(this.inspectionId).subscribe();
     }
 }
