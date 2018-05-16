ebd3d46c2ecaa112460c0429ba0fda0a6635bba5
michael.jolin <michael.jolin@cauca.ca>
2018-05-16 16:43:58 -0400

    feat(#96): Show contact list

--- src/app/inspection-approval/contacts/contacts.component.ts
+++ src/app/inspection-approval/contacts/contacts.component.ts
@@ -1,21 +1,35 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {InspectionService} from '../../inspection-dashboard/shared/services/inspection.service';
 
 @Component({
     selector: 'app-contacts',
     templateUrl: './contacts.component.html',
-    styleUrls: ['./contacts.component.scss']
+    styleUrls: ['./contacts.component.scss'],
+    providers: [
+        InspectionService,
+    ]
 })
 export class ContactsComponent implements OnInit {
     @Input()
     set building(id: string) {
         this.idBuilding = id;
+        this.loadData();
     }
 
     private idBuilding: string;
 
-    constructor() { }
+    contacts: any = {};
+
+    constructor(
+        private inspectionService: InspectionService,
+    ) { }
 
     ngOnInit() {
     }
 
+    loadData() {
+        this.inspectionService.getBuildingContact(this.idBuilding).subscribe(data => {
+            this.contacts = data;
+        });
+    }
 }
