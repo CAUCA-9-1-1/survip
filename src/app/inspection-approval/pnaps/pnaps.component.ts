8effe695c078aa2ceb2bcad565c395c0fc1fd8a2
michael.jolin <michael.jolin@cauca.ca>
2018-05-16 16:54:01 -0400

    feat(#94): Show PNAPS list

--- src/app/inspection-approval/pnaps/pnaps.component.ts
+++ src/app/inspection-approval/pnaps/pnaps.component.ts
@@ -1,21 +1,35 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {InspectionService} from '../../inspection-dashboard/shared/services/inspection.service';
 
 @Component({
     selector: 'app-pnaps',
     templateUrl: './pnaps.component.html',
-    styleUrls: ['./pnaps.component.scss']
+    styleUrls: ['./pnaps.component.scss'],
+    providers: [
+        InspectionService,
+    ]
 })
 export class PnapsComponent implements OnInit {
     @Input()
     set building(id: string) {
         this.idBuilding = id;
+        this.loadData();
     }
 
     private idBuilding: string;
 
-    constructor() { }
+    pnaps: any = {};
+
+    constructor(
+        private inspectionService: InspectionService,
+    ) { }
 
     ngOnInit() {
     }
 
+    loadData() {
+        this.inspectionService.getBuildingPNAPS(this.idBuilding).subscribe(data => {
+            this.pnaps = data;
+        });
+    }
 }
