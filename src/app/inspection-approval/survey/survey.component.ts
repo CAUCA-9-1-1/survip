e64ac0789dfc1aab9d0e7c71dbd6937486e9c5a0
michael.jolin <michael.jolin@cauca.ca>
2018-05-17 14:23:17 -0400

    feat(#95): Show survey summary

--- src/app/inspection-approval/survey/survey.component.ts
+++ src/app/inspection-approval/survey/survey.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {InspectionService} from '../../inspection-dashboard/shared/services/inspection.service';
 
 @Component({
-  selector: 'app-survey',
-  templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.scss']
+    selector: 'app-survey',
+    templateUrl: './survey.component.html',
+    styleUrls: ['./survey.component.scss'],
+    providers: [
+        InspectionService,
+    ]
 })
 export class SurveyComponent implements OnInit {
+    @Input() inspectionId: string;
 
-  constructor() { }
+    summary: any = [];
 
-  ngOnInit() {
-  }
+    constructor(
+        private inspectionService: InspectionService,
+    ) { }
+
+    ngOnInit() {
+        this.inspectionService.getSurveySummary(this.inspectionId).subscribe(data => {
+            this.summary = data;
+        });
+    }
 
 }
