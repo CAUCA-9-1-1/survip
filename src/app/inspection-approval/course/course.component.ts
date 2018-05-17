59c3cfb256892d9c656868fe57262ccf7afe21de
michael.jolin <michael.jolin@cauca.ca>
2018-05-17 12:59:47 -0400

    feat(#102): Show courses

--- src/app/inspection-approval/course/course.component.ts
+++ src/app/inspection-approval/course/course.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {InspectionService} from '../../inspection-dashboard/shared/services/inspection.service';
 
 @Component({
-  selector: 'app-course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+    selector: 'app-course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+    @Input() inspectionId: string;
 
-  constructor() { }
+    courses: any = [];
 
-  ngOnInit() {
-  }
+    constructor(
+        private inspectionService: InspectionService,
+    ) { }
 
+    ngOnInit() {
+        this.inspectionService.getCourse(this.inspectionId).subscribe(data => {
+            this.courses = data.sort((a, b) => {
+                return a.description > b.description ? 1 : -1;
+            });
+
+            data.forEach((course, index) => {
+                this.inspectionService.getCourseLane(course.id).subscribe(infos => {
+                    const lanes = infos.lanes.sort((a, b) => {
+                        return a.sequence > b.sequence ? 1 : -1;
+                    });
+
+                    this.courses[index].lanes = lanes;
+                });
+            });
+        });
+    }
 }
