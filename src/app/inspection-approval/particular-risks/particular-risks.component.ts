a5c296d1fbd464183d76b355ab5b3d4b92dda108
michael.jolin <michael.jolin@cauca.ca>
2018-05-23 15:49:46 -0400

    feat(#67): new slideshow

--- src/app/inspection-approval/particular-risks/particular-risks.component.ts
+++ src/app/inspection-approval/particular-risks/particular-risks.component.ts
@@ -18,14 +18,15 @@ export class ParticularRisksComponent implements OnInit {
     @Input()
     set building(id: string) {
         this.idBuilding = id;
-        this.particularRisks = [];
+        this.resetData();
         this.loadData();
     }
 
     private idBuilding: string;
-    private labels: string[];
 
-    particularRisks: any = [];
+    description: string[];
+    pictures: any[] = [];
+    risks: any[] = [];
 
     constructor(
         private inspectionService: InspectionService,
@@ -35,64 +36,70 @@ export class ParticularRisksComponent implements OnInit {
         this.translateService.get([
             'risks.floor', 'risks.foundation', 'risks.wall', 'risks.roof'
         ]).subscribe(data => {
-            this.labels = data;
+            this.description = [
+                data['risks.foundation'],
+                data['risks.floor'],
+                data['risks.wall'],
+                data['risks.roof'],
+            ];
         });
     }
 
     ngOnInit() {
     }
 
+    resetData() {
+        this.risks = [{}, {}, {}, {}];
+        this.pictures = [[], [], [], []];
+    }
+
     loadData() {
         this.particularRisksService.getFoundation(this.idBuilding).subscribe(data => {
-            this.particularRisks[0] = {
-                description: this.labels['risks.foundation'],
-                risks: data,
-                pictures: [],
-            };
+            this.risks[0] = data;
+            const images = [];
 
             this.particularRisksService.getPictures(data.id).subscribe( pictures => {
-                pictures.forEach((image, index) => {
-                    this.particularRisks[0].pictures.push('data:image/jpeg;base64,' + image['pictureData']);
+                pictures.forEach(image => {
+                    images.push('data:image/jpeg;base64,' + image['pictureData']);
                 });
+
+                this.pictures[0] = images;
             });
         });
         this.particularRisksService.getFloor(this.idBuilding).subscribe(data => {
-            this.particularRisks[1] = {
-                description: this.labels['risks.floor'],
-                risks: data,
-                pictures: [],
-            };
+            this.risks[1] = data;
+            const images = [];
 
             this.particularRisksService.getPictures(data.id).subscribe( pictures => {
-                pictures.forEach((image, index) => {
-                    this.particularRisks[1].pictures.push('data:image/jpeg;base64,' + image['pictureData']);
+                pictures.forEach(image => {
+                    images.push('data:image/jpeg;base64,' + image['pictureData']);
                 });
+
+                this.pictures[1] = images;
             });
         });
         this.particularRisksService.getWall(this.idBuilding).subscribe(data => {
-            this.particularRisks[2] = {
-                description: this.labels['risks.wall'],
-                risks: data,
-                pictures: [],
-            };
+            this.risks[2] = data;
+            const images = [];
 
             this.particularRisksService.getPictures(data.id).subscribe( pictures => {
-                pictures.forEach((image, index) => {
-                    this.particularRisks[2].pictures.push('data:image/jpeg;base64,' + image['pictureData']);
+                pictures.forEach(image => {
+                    images.push('data:image/jpeg;base64,' + image['pictureData']);
                 });
+
+                this.pictures[2] = images;
             });
         });
         this.particularRisksService.getRoof(this.idBuilding).subscribe(data => {
-            this.particularRisks[3] = {
-                description: this.labels['risks.roof'],
-                risks: data,
-                pictures: [],
-            };
+            this.risks[3] = data;
+            const images = [];
 
             this.particularRisksService.getPictures(data.id).subscribe( pictures => {
-                pictures.forEach((image, index) => {
-                    this.particularRisks[3].pictures.push('data:image/jpeg;base64,' + image['pictureData']);
+                pictures.forEach(image => {
+                    images.push('data:image/jpeg;base64,' + image['pictureData']);
                 });
+
+                this.pictures[3] = images;
             });
         });
     }
