3361fc34ad686d76d11c6bf04b69a7b86da82eb6
michael.jolin <michael.jolin@cauca.ca>
2018-05-31 11:51:42 -0400

    feat(#135): Add test environment

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    locale: {
+        available: ['fr', 'en'],
+        use: 'fr',
+    },
+    apiUrl: 'http://catestsprevention1.ad.cauca.ca:5555/api/',
+};
