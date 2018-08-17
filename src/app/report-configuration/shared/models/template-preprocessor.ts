8d8f267d78e9660921df196973c727630d9921f7
Julien Landry <julien.r.lan@googlemail.com>
2018-08-17 15:17:55 -0400

    feat(#147): Move placeholder selection to menu bar

--- src/app/report-configuration/shared/models/template-preprocessor.ts
+++ src/app/report-configuration/shared/models/template-preprocessor.ts
@@ -1,5 +1,15 @@
 export const templateCSSPreprocessor = '<style type="text/css">\n' +
   '    /* Minimal styling to center the editor in this sample */\n' +
+  '  div.new-section {' +
+  '  page-break-inside: avoid;' +
+  '}' +
+  'container, .new-section, .new-section table.fixed{' +
+  '  width: 100%;' +
+  '}' +
+  '' +
+  'new-section table.fixed{' +
+  '  table-layout:fixed;' +
+  '}' +
   'table {\n' +
   '    border-collapse: collapse;\n' +
   '    width: 100%;\n' +
