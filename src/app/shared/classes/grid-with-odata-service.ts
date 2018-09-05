6dc6cd3ce841aa395993f6cf61d29905d26d21c4
michael.jolin <michael.jolin@cauca.ca>
2018-09-05 08:05:17 -0400

    fix(#245): Save lane with odata

---
+++ src/app/shared/classes/grid-with-odata-service.ts
@@ -0,0 +1,55 @@
+import validationEngine from 'devextreme/ui/validation_engine';
+
+
+export abstract class GridWithOdataService {
+    public dataSource = [];
+    public validationGroup = 'custom-validation-group-' + (new Date()).getTime();
+
+    protected form: any;
+    protected loadSpecificOpts: any;
+
+    constructor(
+        protected sourceService?: any,
+    ) { }
+
+    public onInitialized(e) {
+        const options = e.component.option('editing');
+
+        if (options.popup) {
+            options.form.validationGroup = this.validationGroup;
+            options.form.onInitialized = (ev) => {
+                this.form = ev.component;
+            };
+
+            e.component.option('editing', options);
+        }
+    }
+
+    public onRowValidating(e) {
+        const validation = validationEngine.validateGroup(this.validationGroup);
+        const panel = this.form.element().querySelector('.dx-tabpanel');
+        const tabs = panel.querySelectorAll('.dx-tabpanel-tabs .dx-tab');
+
+        tabs.forEach(tab => {
+            tab.removeAttribute('style');
+        });
+
+        if (!validation.isValid) {
+            e.isValid = validation.isValid;
+            e.brokenRules.push(validation.brokenRules[0]);
+        }
+
+        if (!e.isValid) {
+            e.brokenRules.forEach(item => {
+                const tabContent = item.validator.element().closest('.dx-multiview-item');
+                const index = Array.from(tabContent.parentNode.children).indexOf(tabContent);
+
+                if (tabs) {
+                    tabs[index].style.background = 'rgba(217, 83, 79, 0.2)';
+                }
+            });
+        }
+    }
+
+    protected abstract setModel(data: any): any;
+}
