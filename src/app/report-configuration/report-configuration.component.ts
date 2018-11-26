ee27d4a41c356e331daf083641644dc8018b4681
Philippe Robert <philippe.robert@cauca.ca>
2018-11-26 08:12:50 -0500

    feat(#204) : Hot fix from code review

--- src/app/report-configuration/report-configuration.component.ts
+++ src/app/report-configuration/report-configuration.component.ts
@@ -7,81 +7,65 @@ import {PlaceholderGroup} from './shared/models/placeholder-group';
 
 
 @Component({
-  selector: 'app-report-configuration',
-  templateUrl: './report-configuration.component.html',
-  styleUrls: ['./report-configuration.component.scss'],
-  providers: []
+    selector: 'app-report-configuration',
+    templateUrl: './report-configuration.component.html',
+    styleUrls: ['./report-configuration.component.scss'],
+    providers: []
 })
 export class ReportConfigurationComponent implements OnInit {
-  selectedTemplate: ConfigurationTemplate;
-  templateIdentifiers: ConfigurationTemplate[];
-  placeholders: PlaceholderGroup[];
+    public selectedTemplate: ConfigurationTemplate;
+    public templateIdentifiers: ConfigurationTemplate[];
+    public placeholders: PlaceholderGroup[];
 
-  constructor(
-    private activeRoute: ActivatedRoute,
-    private translateService: TranslateService,
-    private reportConfigurationService: ReportTemplateService,
-    private changeDetectorRef: ChangeDetectorRef,
-  ) {   
-   }
-
-  ngOnInit() {
-    this.reportConfigurationService.getPlaceholderList().subscribe(res => {
-      this.placeholders = res;
-    });
-    this.templateIdentifiers = [];
-    this.fetchRequiredData();
-
-    this.selectedTemplate = new ConfigurationTemplate();
-  }
-
-  fetchRequiredData(): void {
-    this.reportConfigurationService.getTemplateList().subscribe(data => {
-      data.forEach((templateIdentifier) => {
-        this.templateIdentifiers.push(templateIdentifier);
-      });
-
-      this.activeRoute.params.subscribe(param => {
-        this.reportConfigurationService.getTemplate(param.idReport).subscribe(data => {
-            this.selectedTemplate = data;
-            this.fetchTemplateData();
-        });
-      });
-    });
-  }
-
-  fetchTemplateData(): void {
-    this.reportConfigurationService.getTemplate(this.selectedTemplate.id).subscribe( res => {
-      this.selectedTemplate.data = res.data;
-      this.changeDetectorRef.detectChanges();
-    });
-    return;
-  }
-
-  saveTemplate(): void {
-    this.reportConfigurationService.saveTemplate(this.selectedTemplate).subscribe(res => {
-      if (this.selectedTemplate.id == null) {
-        this.selectedTemplate.id = res.id;
-      }
-    });
-  }
-
-  loadTemplate(): void {
-    this.fetchTemplateData();
-  }
-
-  selectOrCreateTemplate(template: ConfigurationTemplate) {
-    if (template == null) {
-      return;
-    } else {
-      if (template.id != null) {
-        this.selectedTemplate = template;
-        this.fetchTemplateData();
-      } else {
-        this.templateIdentifiers.push(template);
-        this.selectedTemplate = template;
-        this.saveTemplate();
-      }
+    constructor(
+        private activeRoute: ActivatedRoute,
+        private translateService: TranslateService,
+        private reportConfigurationService: ReportTemplateService,
+        private changeDetectorRef: ChangeDetectorRef) {
     }
-  }
+
+    public ngOnInit() {
+        this.reportConfigurationService.getPlaceholderList().subscribe(res => {
+            this.placeholders = res;
+        });
+        this.templateIdentifiers = [];
+        this.fetchRequiredData();
+
+        this.selectedTemplate = new ConfigurationTemplate();
+    }
+
+    public fetchRequiredData(): void {
+        this.reportConfigurationService.getTemplateList().subscribe(data => {
+            data.forEach((templateIdentifier) => {
+                this.templateIdentifiers.push(templateIdentifier);
+            });
+
+            this.activeRoute.params.subscribe(param => {
+                this.reportConfigurationService.getTemplate(param.idReport).subscribe(data => {
+                    this.selectedTemplate = data;
+                    this.fetchTemplateData();
+                });
+            });
+        });
+    }
+
+    public fetchTemplateData(): void {
+        this.reportConfigurationService.getTemplate(this.selectedTemplate.id).subscribe(res => {
+            this.selectedTemplate.data = res.data;
+            this.changeDetectorRef.detectChanges();
+        });
+        return;
+    }
+
+    public saveTemplate(): void {
+        this.reportConfigurationService.saveTemplate(this.selectedTemplate).subscribe(res => {
+            if (this.selectedTemplate.id == null) {
+                this.selectedTemplate.id = res.id;
+            }
+        });
+    }
+
+    public loadTemplate(): void {
+        this.fetchTemplateData();
+    }
 }
