eed9b373e546eb5e7cc1099dcad3a94eef611a16
Philippe Robert <philippe.robert@cauca.ca>
2018-11-26 08:12:50 -0500

    feat(#204) : Hot fix from code review

--- src/app/report-configuration/select-template/select-template.component.ts
+++ src/app/report-configuration/select-template/select-template.component.ts
@@ -6,97 +6,90 @@ import {ConfigurationTemplate} from '../../shared/models/configuration-template.
 import {ReportTemplateService} from '../../shared/services/report-template.service';
 
 @Component({
-  selector: 'app-template-selection',
-  templateUrl: './select-template.component.html',
-  styleUrls: ['./select-template.component.css']
+    selector: 'app-template-selection',
+    templateUrl: './select-template.component.html',
+    styleUrls: ['./select-template.component.css']
 })
 export class SelectTemplateComponent implements OnInit {
 
-  form: FormGroup;
-  dataSource: any = {};
-  labels = {};
-  templateIdentifiers: ConfigurationTemplate[];
-  isOpenDisabled: boolean;
-  angularIsLoaded = false;
-  editedTemplate: ConfigurationTemplate;
+    public form: FormGroup;
+    public dataSource: any = {};
+    public labels = {};
+    public templateIdentifiers: ConfigurationTemplate[];
+    public isOpenDisabled: boolean;
+    public angularIsLoaded = false;
+    public editedTemplate: ConfigurationTemplate;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private translateService: TranslateService,
-    private reportConfigurationService: ReportTemplateService
-  ) {
-    this.templateIdentifiers = [];
-    this.reportConfigurationService.getTemplateList().subscribe(data => {
-      data.forEach((templateIdentifier) => {
-        this.templateIdentifiers.push(templateIdentifier);
-      });
-    });
-    this.dataSource = this.templateIdentifiers;
-    this.editReport = this.editReport.bind(this);
-  }
-
-  ngOnInit() {
-    this.form = this.formBuilder.group([]);
-    this.isOpenDisabled = true;
-
-    this.translateService.get(['edit']).subscribe(labels => {
-      this.labels = labels;
-      this.checkLoadedElement();
-    });
-  }
-
-  private checkLoadedElement(): boolean {
-    if (this.angularIsLoaded && this.labels !== {}) {
-      return true;
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private translateService: TranslateService,
+        private reportConfigurationService: ReportTemplateService
+    ) {
+        this.templateIdentifiers = [];
+        this.reportConfigurationService.getTemplateList().subscribe(data => {
+            data.forEach((templateIdentifier) => {
+                this.templateIdentifiers.push(templateIdentifier);
+            });
+        });
+        this.dataSource = this.templateIdentifiers;
+        this.editReport = this.editReport.bind(this);
     }
-    return false;
-}
 
-  textAreaEmpty(name: string) {
-    this.isOpenDisabled = name.trim().length === 0;
-  }
+    public ngOnInit() {
+        this.form = this.formBuilder.group([]);
+        this.isOpenDisabled = true;
 
-  onInitNewRow(e) {
-    e.data = new ConfigurationTemplate();
-    e.data.data = '';
-    e.data.isActive = true;
-  }
+        this.translateService.get(['edit']).subscribe(labels => {
+            this.labels = labels;
+            this.checkLoadedElement();
+        });
+    }
 
-  onRowInserting(e){
-    this.saveTemplate(e.data);
-  }
+    private checkLoadedElement(): boolean {
+        return this.angularIsLoaded && this.labels !== {};
+    }
 
-  onEditingStart(e){
-    this.editedTemplate = new ConfigurationTemplate();
-    this.editedTemplate.id = e.data.id;
-    this.editedTemplate.isActive = e.data.isActive;
-    this.reportConfigurationService.getTemplate(e.data.id).subscribe( res => {
-      this.editedTemplate.data = res.data;
-    });
-  }
+    public onInitNewRow(e) {
+        e.data = new ConfigurationTemplate();
+        e.data.data = '';
+        e.data.isActive = true;
+    }
 
-  onRowUpdated(e){
-    this.editedTemplate.name = e.data.name;
-    e.data = this.editedTemplate;
-    this.saveTemplate(e.data);
-  }
+    public onRowInserting(e) {
+        this.saveTemplate(e.data);
+    }
 
-  onRowRemoved(e){
-    let selectedTemplate = e.data;
-    selectedTemplate.isActive = false;
-    this.saveTemplate(selectedTemplate);
-  }
+    public onEditingStart(e) {
+        this.editedTemplate = new ConfigurationTemplate();
+        this.editedTemplate.id = e.data.id;
+        this.editedTemplate.isActive = e.data.isActive;
+        this.reportConfigurationService.getTemplate(e.data.id).subscribe(res => {
+            this.editedTemplate.data = res.data;
+        });
+    }
 
-  editReport(report){
-    this.router.navigate(['/report-edition', report.id]);
-  }
+    public onRowUpdated(e) {
+        this.editedTemplate.name = e.data.name;
+        e.data = this.editedTemplate;
+        this.saveTemplate(e.data);
+    }
 
-  saveTemplate(selectedTemplate: ConfigurationTemplate): void {
-    this.reportConfigurationService.saveTemplate(selectedTemplate).subscribe(res => {
-      if (!selectedTemplate.id) {
-        selectedTemplate.id = res.id;
-      }
-    });
-  }
+    public onRowRemoved(e) {
+        const selectedTemplate = e.data;
+        selectedTemplate.isActive = false;
+        this.saveTemplate(selectedTemplate);
+    }
+
+    public editReport(report) {
+        this.router.navigate(['/report-edition', report.id]);
+    }
+
+    public saveTemplate(selectedTemplate: ConfigurationTemplate) {
+        this.reportConfigurationService.saveTemplate(selectedTemplate).subscribe(res => {
+            if (!selectedTemplate.id) {
+                selectedTemplate.id = res.id;
+            }
+        });
+    }
 }
