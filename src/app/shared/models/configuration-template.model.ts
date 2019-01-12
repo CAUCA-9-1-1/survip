38cb9528355fcc40d6e708761de12b5b76b9230d
audrey <audrey.lebelr@gmail.com>
2019-01-11 21:45:37 -0500

    feat(#420) Reports by SSI and default report

--- src/app/shared/models/configuration-template.model.ts
+++ src/app/shared/models/configuration-template.model.ts
@@ -4,6 +4,8 @@ import { BaseModel } from './base.model';
 export class ConfigurationTemplate extends BaseModel {
   data: string;
   name: string;
+  idFireSafetyDepartment: string;
+  isDefault: boolean;
 
   static fromJSON(data: object): ConfigurationTemplate {
     const template = new ConfigurationTemplate();
