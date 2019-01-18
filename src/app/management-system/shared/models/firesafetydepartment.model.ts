ecb3d06afbf90095c96e18c6b9ec1824e9a00e31
Philippe Robert <philippe.robert@cauca.ca>
2019-01-18 11:58:02 -0500

    feat(#243) Add picture to fire safety department

--- src/app/management-system/shared/models/firesafetydepartment.model.ts
+++ src/app/management-system/shared/models/firesafetydepartment.model.ts
@@ -1,9 +1,12 @@
 import { WithLocalization } from '../../../shared/models/with-localization';
+import {Picture} from '../../../shared/models/picture.model';
 
 
 export class FireSafetyDepartment extends WithLocalization {
     idCounty: string;
     language: string;
+    idPicture: string = null;
+    picture: Picture;
 
     static fromJSON(data: object): FireSafetyDepartment {
         const department = new FireSafetyDepartment();
