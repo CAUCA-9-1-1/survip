e6c61bc8de63f8e5e4b3d154bee28a8f59aaead7
audrey <audrey.lebelr@gmail.com>
2019-01-21 02:05:57 -0500

    feat(#425) Refactored code

--- src/app/report-configuration/report-configuration.component.ts
+++ src/app/report-configuration/report-configuration.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ReportTemplateService} from '../shared/services/report-template.service';
 import {TranslateService} from '@ngx-translate/core';
@@ -14,48 +14,31 @@ import {PlaceholderGroup} from './shared/models/placeholder-group';
 })
 export class ReportConfigurationComponent implements OnInit {
     public selectedTemplate: ConfigurationTemplate;
-    public templateIdentifiers: ConfigurationTemplate[];
     public placeholders: PlaceholderGroup[];
 
     constructor(
         private activeRoute: ActivatedRoute,
         private translateService: TranslateService,
-        private reportConfigurationService: ReportTemplateService,
-        private changeDetectorRef: ChangeDetectorRef) {
+        private reportConfigurationService: ReportTemplateService) {
     }
 
     public ngOnInit() {
+        this.selectedTemplate = new ConfigurationTemplate();
+
         this.reportConfigurationService.getPlaceholderList().subscribe(res => {
             this.placeholders = res;
-            console.log('placeHolder : ', this.placeholders);
         });
-        this.templateIdentifiers = [];
-        this.fetchRequiredData();
 
-        this.selectedTemplate = new ConfigurationTemplate();
-    }
-
-    public fetchRequiredData(): void {
-        this.reportConfigurationService.getTemplateList().subscribe(data => {
-            data.forEach((templateIdentifier) => {
-                this.templateIdentifiers.push(templateIdentifier);
-            });
-
-            this.activeRoute.params.subscribe(param => {
-                this.reportConfigurationService.getTemplate(param.idReport).subscribe(data => {
-                    this.selectedTemplate = data;
-                    this.fetchTemplateData();
-                });
-            });
+        this.activeRoute.params.subscribe(param => {
+            //this.selectedTemplate.id = param.idReport;
+            this.loadTemplate(param.idReport);
         });
     }
 
-    public fetchTemplateData(): void {
-        this.reportConfigurationService.getTemplate(this.selectedTemplate.id).subscribe(res => {
-            this.selectedTemplate.data = res.data;
-            this.changeDetectorRef.detectChanges();
+    public loadTemplate(id: string): void {
+        this.reportConfigurationService.getTemplate(id).subscribe(res => {
+            this.selectedTemplate = res;
         });
-        return;
     }
 
     public saveTemplate(): void {
@@ -64,9 +47,5 @@ export class ReportConfigurationComponent implements OnInit {
                 this.selectedTemplate.id = res.id;
             }
         });
-    }
-
-    public loadTemplate(): void {
-        this.fetchTemplateData();
     }
 }
