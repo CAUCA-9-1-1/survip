ec75b6c3ac59417a0e35aabf1f2ecf96b7e55a4a
Audrey Lebel <audrey.lebelr@gmail.com>
2019-05-05 13:16:32 -0400

    Feat(#404) Smoother update when changing SSI

--- src/app/statistics/shared/models/inspection-for-statistics.model.ts
+++ src/app/statistics/shared/models/inspection-for-statistics.model.ts
@@ -4,8 +4,11 @@ export class InspectionForStatistics extends BaseModel {
     public id: string;
     public status: number;
     public riskLevel: number;
-    public idCity: string;
+    public idFireSafetyDepartment: string;
     public completedOn: Date;
+    public hasBeenRefused: boolean;
+    public ownerWasAbsent: boolean;
+    public doorHangerHasBeenLeft: boolean;
 
     static fromJSON(data: object): InspectionForStatistics {
         const inspection = new InspectionForStatistics();
